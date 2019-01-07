26f772238c5731aa543f561f0fc6946199a32561
bi xu <bixu@bideiMac-2.local>
2019-01-07 17:46:20 +0800

    1

---
+++ ZXSArcStepSlider/ZXSArcStepSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXSArcStepSlider
+//
+//  Created by bi xu on 2019/1/7.
+//  Copyright © 2019 Cammus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXSArcStepSlider/ZXSArcStepSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXSArcStepSlider
+//
+//  Created by bi xu on 2019/1/7.
+//  Copyright © 2019 Cammus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
