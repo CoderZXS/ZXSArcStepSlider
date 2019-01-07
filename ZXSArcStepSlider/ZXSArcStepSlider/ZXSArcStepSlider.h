c8d127a08cc5a74e92c1866916172d0b63ec8b93
bi xu <bixu@bideiMac-2.local>
2019-01-07 17:58:30 +0800

    1

---
+++ ZXSArcStepSlider/ZXSArcStepSlider/ZXSArcStepSlider.h
@@ -0,0 +1,17 @@
+//
+//  ZXSArcStepSlider.h
+//  ZXSArcStepSlider
+//
+//  Created by bi xu on 2019/1/7.
+//  Copyright © 2019 Cammus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZXSArcStepSlider : UIControl
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZXSArcStepSlider/ZXSArcStepSlider/ZXSArcStepSlider.m
@@ -0,0 +1,21 @@
+//
+//  ZXSArcStepSlider.m
+//  ZXSArcStepSlider
+//
+//  Created by bi xu on 2019/1/7.
+//  Copyright © 2019 Cammus. All rights reserved.
+//
+
+#import "ZXSArcStepSlider.h"
+
+@implementation ZXSArcStepSlider
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
