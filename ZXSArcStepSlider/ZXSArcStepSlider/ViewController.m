7747f0731dd5b6dda845e91b29ba468082a08496
bi xu <bixu@bideiMac-2.local>
2019-01-09 16:29:34 +0800

    1

--- ZXSArcStepSlider/ZXSArcStepSlider/ViewController.m
+++ ZXSArcStepSlider/ZXSArcStepSlider/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "ZXSArcStepSlider.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UILabel *label;
 
 @end
 
@@ -16,8 +19,104 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.label = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, 375, 375)];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    self.label.font = [UIFont systemFontOfSize:150];
+    self.label.textColor = [UIColor redColor];
+    self.label.text = @"16";
+    [self.view addSubview:self.label];
+    
+    ZXSArcStepSlider *slder = [[ZXSArcStepSlider alloc] initWithFrame:CGRectMake(0, 100, 375, 375)];
+    [self.view addSubview:slder];
+    slder.startAngle = M_PI_4 * 3;
+    slder.color = [UIColor colorWithRed:29.0 / 255.0 green:207.0 / 255.0 blue:0.0 alpha:1.0];
+    slder.maxValue = 19;
+    slder.startValue = 0;
+    slder.endValue = 14;
+    slder.sectorsRadius = 135;
+    [slder addTarget:self action:@selector(valueChange:) forControlEvents:UIControlEventValueChanged];
+    
+    slder.drowNumber = ^(CGFloat radius, CGFloat x, CGFloat y) {
+        NSArray *angleArray = @[@36, @18, @0];
+        NSInteger num = 16;
+        for (NSNumber *number in angleArray) {
+            CGFloat angle = number.floatValue;
+            CGPoint point = [self getPointWithAngle:angle radius:radius];
+            CGFloat xx = x - point.x;
+            CGFloat yy = y + point.y;
+            CGRect textRect = CGRectMake(xx - 10, yy - 6, 20, 12);
+            NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
+            paragraph.alignment = NSTextAlignmentCenter;
+            NSDictionary *dic = @{NSFontAttributeName : [UIFont systemFontOfSize:12],
+                                  NSForegroundColorAttributeName : [UIColor blackColor],
+                                  NSParagraphStyleAttributeName : paragraph};
+            [[NSString stringWithFormat:@"%ld",(long)num] drawInRect:textRect withAttributes:dic];
+            num++;
+        }
+        angleArray = @[@18, @36, @54, @72, @90];
+        
+        for (NSNumber *number in angleArray) {
+            CGFloat angle = number.floatValue;
+            CGPoint point = [self getPointWithAngle:angle radius:radius];
+            CGFloat xx = x-point.x;
+            CGFloat yy = y-point.y;
+            CGRect textRect = CGRectMake(xx - 10, yy - 6, 20, 12);
+            NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
+            paragraph.alignment = NSTextAlignmentCenter;
+            NSDictionary *dic = @{NSFontAttributeName : [UIFont systemFontOfSize:12],
+                                  NSForegroundColorAttributeName : [UIColor blackColor],
+                                  NSParagraphStyleAttributeName : paragraph};
+            [[NSString stringWithFormat:@"%ld",(long)num] drawInRect:textRect withAttributes:dic];
+            num++;
+        }
+        angleArray = @[@72, @54, @36, @18, @0];
+        
+        for (NSNumber *number in angleArray) {
+            CGFloat angle = number.floatValue;
+            CGPoint point = [self getPointWithAngle:angle radius:radius];
+            CGFloat xx = x + point.x;
+            CGFloat yy = y - point.y;
+            CGRect textRect = CGRectMake(xx - 10, yy - 6, 20, 12);
+            NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
+            paragraph.alignment = NSTextAlignmentCenter;
+            NSDictionary *dic = @{NSFontAttributeName : [UIFont systemFontOfSize:12],
+                                  NSForegroundColorAttributeName : [UIColor blackColor],
+                                  NSParagraphStyleAttributeName : paragraph};
+            [[NSString stringWithFormat:@"%ld",(long)num] drawInRect:textRect withAttributes:dic];
+            num++;
+        }
+        angleArray = @[@18, @36];
+        
+        for (NSNumber *number in angleArray) {
+            CGFloat angle = number.floatValue;
+            CGPoint point = [self getPointWithAngle:angle radius:radius];
+            CGFloat xx = x + point.x;
+            CGFloat yy = y + point.y;
+            CGRect textRect = CGRectMake(xx - 10, yy - 6, 20, 12);
+            NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
+            paragraph.alignment = NSTextAlignmentCenter;
+            NSDictionary *dic = @{NSFontAttributeName : [UIFont systemFontOfSize:12],
+                                  NSForegroundColorAttributeName : [UIColor blackColor],
+                                  NSParagraphStyleAttributeName : paragraph};
+            [[NSString stringWithFormat:@"%ld",(long)num] drawInRect:textRect withAttributes:dic];
+            num++;
+        }
+    };
 }
+- (void)valueChange:(ZXSArcStepSlider *)slder {
+    self.label.text = [NSString stringWithFormat:@"%d",(int)slder.startValue + 16];
+}
+
+//通过角度获得x,y值
+- (CGPoint)getPointWithAngle:(CGFloat)angle radius:(CGFloat)r {
+    CGFloat y = r * sin(angle * M_PI / 180.0);
+    CGFloat x = r * cos(angle * M_PI / 180.0);
+    return CGPointMake(x, y);
+}
+
 
 
 @end
--- ZXSArcStepSlider/ZXSArcStepSlider/ZXSArcStepSlider.m
+++ ZXSArcStepSlider/ZXSArcStepSlider/ZXSArcStepSlider.m
@@ -8,14 +8,325 @@
 
 #import "ZXSArcStepSlider.h"
 
+// 整个slider需要的信息参数结构体
+typedef struct {
+    double radius;
+    double angle;
+} YHPolarCoordinate;
+
+@interface ZXSArcStepSlider ()
+
+@property (nonatomic, assign) CGPoint circleCenter;
+@property (nonatomic, assign) CGFloat radius;
+
+@property (nonatomic, assign) double fullLine;
+@property (nonatomic, assign) double circleOffset;
+@property (nonatomic, assign) double circleLine;
+@property (nonatomic, assign) double circleEmpty;
+
+@property (nonatomic, assign) double circleOffsetAngle;
+@property (nonatomic, assign) double circleLineAngle;
+@property (nonatomic, assign) double circleEmptyAngle;
+
+@property (nonatomic, assign) CGPoint startMarkerCenter;
+@property (nonatomic, assign) CGPoint endMarkerCenter;
+
+@property (nonatomic, assign) CGFloat startMarkerRadius;
+@property (nonatomic, assign) CGFloat endMarkerRadius;
+
+@property (nonatomic, assign) CGFloat startMarkerFontSize;
+@property (nonatomic, assign) CGFloat endMarkerFontize;
+
+@property (nonatomic, assign) CGFloat startMarkerAlpha;
+@property (nonatomic, assign) CGFloat endMarkerAlpha;
+
+@property (nonatomic, assign) BOOL trackingSectorStartMarker;
+
+@end
+
+
 @implementation ZXSArcStepSlider
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - 系统
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self setupInit];
+    }
+    return self;
 }
-*/
+
+- (void)drawRect:(CGRect)rect {
+    [self drawSector];
+}
+
+//开始
+- (BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
+    CGPoint touchPoint = [touch locationInView:self];
+    [self sectorToDrawInf];
+    
+    if ([self touchInCircleWithPoint:touchPoint circleCenter:self.endMarkerCenter]) {
+        self.trackingSectorStartMarker = NO;
+        return YES;
+    }
+    
+    if ([self touchInCircleWithPoint:touchPoint circleCenter:self.startMarkerCenter]) {
+        self.trackingSectorStartMarker = YES;
+        return YES;
+    }
+    
+    return NO;
+}
+
+//持续
+- (BOOL)continueTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
+    CGPoint touchPoint = [touch locationInView:self];
+    CGPoint ceter = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+    YHPolarCoordinate polar = decartToPolar(ceter, touchPoint);
+    
+    double correctedAngle;
+    if(polar.angle < self.startAngle) correctedAngle = polar.angle + (2 * M_PI - self.startAngle);
+    else correctedAngle = polar.angle - self.startAngle;
+    
+    double procent = correctedAngle / (M_PI * 2);
+    
+    double newValue = procent * (self.maxValue - self.minValue) + self.minValue;
+    
+    if (self.trackingSectorStartMarker) {
+        if (newValue > self.startValue) {
+            double diff = newValue - self.startValue;
+            if (diff > ((self.maxValue - self.minValue)/2)) {
+                self.startValue = self.minValue;
+                [self valueChangedNotification];
+                [self setNeedsDisplay];
+                return YES;
+            }
+        }
+        
+        if (newValue >= self.endValue) {
+            self.startValue = self.endValue;
+            [self valueChangedNotification];
+            [self setNeedsDisplay];
+            return YES;
+        }
+        
+        self.startValue = newValue;
+        [self valueChangedNotification];
+        
+    } else {
+        if (newValue < self.endValue) {
+            double diff = self.endValue - newValue;
+            if (diff > ((self.maxValue - self.minValue)/2)) {
+                self.endValue = self.maxValue;
+                [self valueChangedNotification];
+                [self setNeedsDisplay];
+                return YES;
+            }
+        }
+        
+        if (newValue <= self.startValue) {
+            self.endValue = self.startValue;
+            [self valueChangedNotification];
+            [self setNeedsDisplay];
+            return YES;
+        }
+        
+        self.endValue = newValue;
+        [self valueChangedNotification];
+    }
+    
+    [self setNeedsDisplay];
+    return YES;
+}
+
+//结束
+- (void)endTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
+    self.trackingSectorStartMarker = NO;
+}
+
+
+#pragma mark - 自定义
+
+- (void)setupInit {
+    self.minValue = 0.0;
+    self.maxValue = 100.0;
+    self.startValue = 0.0;
+    self.endValue = 50.0;
+    self.color = [UIColor greenColor];
+    
+    self.sectorsRadius = 45.0;
+    self.backgroundColor = [UIColor clearColor];
+    self.startAngle = toRadians(135);
+    self.markRadius = 20;
+    self.circleLineWidth = 20;
+    self.lineWidth = 2;
+}
+
+- (void)setSectorsRadius:(double)sectorsRadius {
+    _sectorsRadius = sectorsRadius;
+    [self setNeedsDisplay];
+}
+
+- (void)drawSector {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, self.circleLineWidth);
+    
+    UIColor *startCircleColor = self.color;
+    UIColor *markBackcolor = [UIColor whiteColor];
+    
+    [self sectorToDrawInf];
+    CGFloat x = self.circleCenter.x;
+    CGFloat y = self.circleCenter.y;
+    CGFloat r = self.radius;
+    
+    //start circle line
+    [startCircleColor setStroke];
+    
+    CGContextAddArc(context, x, y, r, self.startAngle, self.circleOffsetAngle, 0);
+    CGContextSetLineCap(context, kCGLineCapRound);
+    CGContextStrokePath(context);
+    
+    //clearing place for start marker
+    CGContextSaveGState(context);
+    CGContextAddArc(context, self.startMarkerCenter.x, self.startMarkerCenter.y, self.startMarkerRadius - (self.lineWidth/2.0), 0.0, 6.28, 0);
+    CGContextClip(context);
+    CGContextClearRect(context, self.bounds);
+    CGContextRestoreGState(context);
+    
+    //clearing place for end marker
+    CGContextSaveGState(context);
+    CGContextAddArc(context, self.endMarkerCenter.x, self.endMarkerCenter.y, self.endMarkerRadius - (self.lineWidth/2.0), 0.0, 6.28, 0);
+    CGContextClip(context);
+    CGContextClearRect(context, self.bounds);
+    CGContextRestoreGState(context);
+    
+    CGFloat len = r / sqrt(2);
+    
+    //外圆弧
+    CGContextSetLineWidth(context, self.lineWidth);
+    CGContextAddArc(context, x, y, r + 10, self.startAngle, M_PI_4, 0);
+    CGContextStrokePath(context);
+    
+    //左端点
+    CGContextSaveGState(context);
+    CGContextAddArc(context, x - len, y + len, 10, -M_PI_4, M_PI_4*3, 0);
+    CGContextStrokePath(context);
+    
+    //内圆弧
+    CGContextSaveGState(context);
+    CGContextAddArc(context, x, y, r - 10, self.startAngle, M_PI_4, 0);
+    CGContextStrokePath(context);
+    //右端点
+    CGContextSaveGState(context);
+    CGContextAddArc(context, x + len, y + len, 10, M_PI_4, M_PI_4*5, 0);
+    CGContextStrokePath(context);
+    
+    //如果需要圆弧上面有字
+    if (self.drowNumber) {
+        self.drowNumber(r,x,y);
+    }
+    
+    //标记
+    CGContextSetLineWidth(context, self.lineWidth);
+    [[startCircleColor colorWithAlphaComponent:self.startMarkerAlpha] setStroke];
+    CGContextAddArc(context, self.startMarkerCenter.x, self.startMarkerCenter.y, self.startMarkerRadius, 0.0, 6.28, 0);
+    //标记背景色
+    CGContextStrokePath(context);
+    [markBackcolor setFill];
+    [[startCircleColor colorWithAlphaComponent:self.startMarkerAlpha] setStroke];
+    CGContextAddArc(context, self.startMarkerCenter.x, self.startMarkerCenter.y, self.startMarkerRadius - 1, 0.0, 6.28, 0);
+    CGContextFillPath(context);
+    //标记上面的字
+    NSString *startMarkerStr = [NSString stringWithFormat:@"%.0f", self.startValue + 16];
+    [self drawString:startMarkerStr
+            withFont:self.startMarkerFontSize
+               color:[startCircleColor colorWithAlphaComponent:self.startMarkerAlpha]
+          withCenter:self.startMarkerCenter];
+}
+
+- (void)sectorToDrawInf {
+    self.circleCenter = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height /2);
+    self.radius = self.sectorsRadius;// 圆半径
+    
+    self.fullLine = self.maxValue - self.minValue;
+    self.circleOffset = self.startValue - self.minValue;
+    self.circleLine = self.endValue - self.startValue;
+    self.circleEmpty = self.maxValue - self.endValue;
+    
+    self.circleOffsetAngle = (self.circleOffset / self.fullLine) * M_PI * 2 + self.startAngle;
+    self.circleLineAngle = (self.circleLine / self.fullLine) * M_PI * 2 + self.circleOffsetAngle;
+    self.circleEmptyAngle = M_PI * 2 + self.startAngle;
+    
+    self.startMarkerCenter = polarToDecart(self.circleCenter, self.radius, self.circleOffsetAngle);
+    
+    self.startMarkerRadius = self.markRadius;
+    
+    self.startMarkerFontSize = 18;
+    self.startMarkerAlpha = 1.0;
+}
+
+//mark上面的字
+- (void)drawString:(NSString *)s withFont:(CGFloat)fontSize color:(UIColor *)color withCenter:(CGPoint)center {
+    NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
+    paragraph.alignment = NSTextAlignmentCenter;
+    NSDictionary *dic = @{NSFontAttributeName : [UIFont systemFontOfSize:fontSize],
+                          NSForegroundColorAttributeName : color,
+                          NSParagraphStyleAttributeName : paragraph};
+    
+    CGFloat x = center.x - (self.markRadius);
+    CGFloat y = center.y - (self.markRadius / 2);
+    CGRect textRect = CGRectMake(x, y, self.markRadius * 2, self.markRadius);
+    
+    [s drawInRect:textRect withAttributes:dic];
+}
+
+//判断点击的位置是否是mark内
+- (BOOL)touchInCircleWithPoint:(CGPoint)touchPoint circleCenter:(CGPoint)circleCenter {
+    YHPolarCoordinate polar = decartToPolar(circleCenter, touchPoint);
+    return polar.radius < self.markRadius;
+}
+
+- (void)valueChangedNotification {
+    [self sendActionsForControlEvents:UIControlEventValueChanged];
+}
+
+CGFloat toDegrees(CGFloat radians) {
+    return radians * 180 / M_PI;
+}
+
+CGFloat toRadians(CGFloat degrees) {
+    return degrees * M_PI / 180;
+}
+
+CGFloat segmentAngle(CGPoint startPoint, CGPoint endPoint) {
+    CGPoint v = CGPointMake(endPoint.x - startPoint.x, endPoint.y - startPoint.y);
+    float vmag = sqrt(powf(v.x, 2.0) + powf(v.y, 2.0));
+    v.x /= vmag;
+    v.y /= vmag;
+    double radians = atan2(v.y, v.x);
+    return radians;
+}
+
+CGFloat segmentLength(CGPoint startPoint, CGPoint endPoint) {
+    return decartToPolar(startPoint, endPoint).radius;
+}
+
+CGPoint polarToDecart(CGPoint startPoint, CGFloat radius, CGFloat angle) {
+    CGFloat x = radius * cos(angle) + startPoint.x;
+    CGFloat y = radius * sin(angle) + startPoint.y;
+    return CGPointMake(x, y);
+}
+
+YHPolarCoordinate decartToPolar(CGPoint center, CGPoint point) {
+    double x = point.x - center.x;
+    double y = point.y - center.y;
+    
+    YHPolarCoordinate polar;
+    polar.radius = sqrt(pow(x, 2.0) + pow(y, 2.0));
+    polar.angle = acos(x / (sqrt(pow(x, 2.0) + pow(y, 2.0))));
+    if(y < 0) polar.angle = 2 * M_PI - polar.angle;
+    return polar;
+}
+
 
 @end
